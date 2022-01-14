e4760b007fcccb508784be496f2d3463f5bd7fd4
Tomasz Nowak <tomeknew1@gmail.com>
2022-01-14 22:18:02 +0000

    Classes Name and Person implemented

--- classes/Name.java
+++ classes/Name.java
@@ -1,5 +1,43 @@
 package classes;
 
 public class Name {
+	protected String firstName, lastName;
 
+	public Name(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String toString() {
+		String fullName = "Firstname: " + this.firstName + ", Lastname: " + this.lastName;
+		return fullName;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this.firstName == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Name name = (Name) o;
+		boolean result = this.firstName.toLowerCase().equals(name.firstName.toLowerCase())
+				&& this.lastName.toLowerCase().equals(name.lastName.toLowerCase());
+		return result;
+	}
 }
--- classes/Person.java
+++ classes/Person.java
@@ -1,5 +1,42 @@
 package classes;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	private Name name;
+	protected String PersonID;
+	private String phone;
+
+	public Person(Name name, String id, String phone) {
+		this.name = name;
+		this.PersonID = id;
+		this.phone = phone;
+	}
+
+	public String getID() {
+		return PersonID;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setId(String id) {
+		this.PersonID = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String toString() {
+		String str = this.getName().getLastName() + ", " + this.getName().getFirstName() + " ID:" + this.PersonID
+				+ ", phone:" + this.phone;
+		return str;
+	}
 
 }
