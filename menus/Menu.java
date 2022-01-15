14884fec3b934a1725105b9f96ff32c8cad57f3b
Tomasz Nowak <tomeknew1@gmail.com>
2022-01-15 20:28:38 +0000

    Some options of menu implemented

--- menus/Menu.java
+++ menus/Menu.java
@@ -1,5 +1,15 @@
 package menus;
 
+import java.time.LocalDate;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import classes.Consultant;
+import classes.Name;
+import classes.Patient;
+import classes.Practice;
+import classes.SeverityOptions;
+import classes.Visit;
 import collections.ConsultantsList;
 import controller.AppController;
 
@@ -33,15 +43,39 @@ public class Menu {
 			switch (option) {
 			case RecordVisit:
 				System.out.println("Record a Patient visit.");
+				System.out.println("Who is a patient consultant?");
+				Consultant c = practice.find();
+				System.out.println("Select a patient from the list");
+				c.showPatients();
+				int patientChoice = 0;
+				boolean inputValidationP = false;
+				while (!inputValidationP) {
+					try {
+						patientChoice = scanner.nextInt();
+						if (patientChoice < 1 || patientChoice > c.getPatients().size()) {
+							throw new InputMismatchException();
+						}
+						inputValidationP = true;
+					} catch (InputMismatchException ex) {
+						System.out.println("Please enter correct number of patient.");
+					}
+					scanner.nextLine();
+				}
+				Patient p = c.getPatients().get(patientChoice - 1);
+				System.out.println("Add Notes to the visit: ");
+				String notes = scanner.nextLine();
+				c.addVisit(p, new Visit(now, notes));
 
 				break;
 			case DisplayAllPatientsofConsultant:
 				System.out.println("Display all patients of a particular Consultant.");
-
+				System.out.println("Choose a consultant");
+				Consultant op2 = practice.find();
+				op2.showPatients();
 				break;
 			case DisplayAllCon:
 				System.out.println("Display all Consultants followed by their Patients followed by their visits.");
-
+				practice.showDetails();
 				break;
 			case LoadInfo:
 				System.out.println("Load information from a file");
@@ -49,7 +83,6 @@ public class Menu {
 				break;
 			case Quit:
 				System.out.println("Thank you, have a nice day");
-
 				System.exit(0);
 			}
 
